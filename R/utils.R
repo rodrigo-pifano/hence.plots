026925b9b0da0ec92e40f6d0027def3f53aa5dd5
Rodrigo Pifano <pifanorodrigo@gmail.com>
2022-10-17 15:27:35 -0300

    .

--- R/utils.R
+++ R/utils.R
@@ -1,14 +1,7 @@
 # Auxiliary functions ----------------------------------------------------------
 
-#' check whether an object is null, but returns FALSE if error occurs
-#'
-#' @return
-#' @export
-#'
-#' @examples
-safe_null <- function(x) {
-  purrr::safely(is.null(x), otherwise = FALSE)
-}
+
+safe_null <- purrr::safely(is.null, otherwise = FALSE)
 
 aes <- ggplot2::aes
 
